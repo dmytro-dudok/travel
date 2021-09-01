8b1c7c73a86f9892dc407c3f81a19ea723fb3c7c
dmytro-dudok <ddudok65@gmail.com>
2021-09-01 21:08:17 +0300

    add all mockup

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,128 @@
-$(function(){
+$(function () {
+	$('.destinations__slider').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		prevArrow:
+			'<button class="slider-btn slider-btn__left"><i class="fas fa-angle-left"></i></button>',
+		nextArrow:
+			'<button class="slider-btn slider-btn__right"><i class="fas fa-angle-right"></i></button>',
+	})
+})
 
+const count = document.querySelector('.booking__content-text-2 span')
+const likeBtn = document.querySelector('.booking__like-btn')
 
+likeBtn.addEventListener('click', () => {
+	let likes = parseInt(count.textContent)
+	likes++
+	count.textContent = likes
+})
 
-  
-});
+window.addEventListener('load', () => {
+	const content = document.querySelector('.reviews__content')
+	const sliderLength = document.querySelectorAll(
+		'.reviews__slider-item'
+	).length
+})
+
+const dotsContainer = document.querySelector('.reviews__dots-container')
+const slides = document.querySelectorAll('.reviews__slider-item')
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+let dots = document.querySelectorAll('.reviews__dot')
+
+let current = 0
+let prev = 0
+let next = 1
+
+const getNext = () => {
+	if(current === slides.length-1){
+		next = 0
+	}
+	next++
+	return next
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+	prevBtn.classList.add('disabled')
+	nextBtn.classList.add('btn--active')
+
+	dotsContainer.innerHTML = displayDots()
+
+	dots = document.querySelectorAll('.reviews__dot')
+	dots[current].classList.add('reviews__dot--active')
+	slides[current].style.transform = 'translate3d(-48px, -77px, 10px)'
+
+	dots.forEach((dot, index) => {
+		dot.addEventListener('click', () => {
+			current = index
+			next = getCurrent()
+			console.log(`index`, index);
+			carousel()
+			prev = current
+		})
+	})
+})
+
+let displayDots = () => {
+	let dots = ''
+	for (let i = 0; i < slides.length; i++) {
+		dots += '<button class="reviews__dot"></button>'
+	}
+	return dots
+}
+
+nextBtn.addEventListener('click', () => {
+	current++
+	next = getCurrent()
+	carousel()
+	prev = current
+})
+
+prevBtn.addEventListener('click', () => {
+	next = current
+	current--
+	carousel()
+	prev = current
+})
+
+const carousel = () => {
+	if (current === 0) {
+		prevBtn.classList.remove('btn--active')
+		prevBtn.classList.add('disabled')
+		nextBtn.classList.add('btn--active')
+		nextBtn.classList.remove('disabled')
+	} else if ( current < slides.length - 1) {
+		prevBtn.classList.add('btn--active')
+		prevBtn.classList.remove('disabled')
+		nextBtn.classList.add('btn--active')
+		nextBtn.classList.remove('disabled')
+	}
+	else if(current === slides.length - 1){
+		prevBtn.classList.add('btn--active')
+		prevBtn.classList.remove('disabled')
+		nextBtn.classList.remove('btn--active')
+		nextBtn.classList.add('disabled')
+	} else {
+		nextBtn.classList.add('disabled')
+		nextBtn.classList.remove('btn--active')
+	}
+	console.log(prevBtn.classList);
+	console.log(nextBtn.classList);
+
+	dots.forEach((dot) => {
+		dot.classList.remove('reviews__dot--active')
+	})
+	dots[current].classList.add('reviews__dot--active')
+	console.log(`prev`, prev)
+	console.log(`current`, current)
+	slides[prev].style.transform = 'translate3d(0, 0, 0)'
+	slides[prev].style.zIndex = `0`
+	slides[current].style.transform = 'translate3d(-48px, -77px, 10px)'
+	slides[current].style.zIndex = `${slides.length}`
+	slides[next].style.zIndex = `${slides.length - 1}`
+
+	console.log(`z`, slides[prev].style.zIndex);
+	console.log(`zc`, slides[current].style.zIndex);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,93 @@
-let gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    del = require('del'),
-    autoprefixer = require('gulp-autoprefixer');
+const { src, dest, watch, parallel, series }  = require('gulp');
 
+const scss          = require('gulp-sass')(require('sass'));
+const concat        = require('gulp-concat');
+const browserSync   = require('browser-sync').create();
+const uglify        = require('gulp-uglify-es').default;
+const autoprefixer  = require('gulp-autoprefixer');
+const imagemin      = require('gulp-imagemin');
+const del           = require('del');
 
-gulp.task('clean', async function(){
-  del.sync('dist')
-})
-
-gulp.task('scss', function(){
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(autoprefixer({
-      browsers: ['last 8 versions']
-    }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('css', function(){
-  return gulp.src([
-    'node_modules/normalize.css/normalize.css',
-    'node_modules/slick-carousel/slick/slick.css',
-  ])
-    .pipe(concat('_libs.scss'))
-    .pipe(gulp.dest('app/scss'))
-    .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('html', function(){
-  return gulp.src('app/*.html')
-  .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('script', function(){
-  return gulp.src('app/js/*.js')
-  .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('js', function(){
-  return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
-  ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
-    .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('browser-sync', function() {
+function browsersync() {
   browserSync.init({
-      server: {
-          baseDir: "app/"
-      }
+    server : {
+      baseDir: 'app/'
+    }
   });
-});
+}
 
-gulp.task('export', function(){
-  let buildHtml = gulp.src('app/**/*.html')
-    .pipe(gulp.dest('dist'));
+function cleanDist() {
+  return del('dist')
+}
 
-  let BuildCss = gulp.src('app/css/**/*.css')
-    .pipe(gulp.dest('dist/css'));
+function images() {
+  return src('app/images/**/*')
+    .pipe(imagemin(
+      [
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            { removeViewBox: true },
+            { cleanupIDs: false }
+          ]
+        })
+      ]
+    ))
+    .pipe(dest('dist/images'))
+}
 
-  let BuildJs = gulp.src('app/js/**/*.js')
-    .pipe(gulp.dest('dist/js'));
-    
-  let BuildFonts = gulp.src('app/fonts/**/*.*')
-    .pipe(gulp.dest('dist/fonts'));
+function scripts() {
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
+    'app/js/main.js'
+  ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
+}
 
-  let BuildImg = gulp.src('app/img/**/*.*')
-    .pipe(gulp.dest('dist/img'));   
-});
 
-gulp.task('watch', function(){
-  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
-  gulp.watch('app/*.html', gulp.parallel('html'))
-  gulp.watch('app/js/*.js', gulp.parallel('script'))
-});
+function styles() {
+  return src('app/scss/style.scss')
+      .pipe(scss({outputStyle: 'compressed'}))
+      .pipe(concat('style.min.css'))
+      .pipe(autoprefixer({
+        overrideBrowserslist: ['last 10 version'],
+        grid: true
+      }))
+      .pipe(dest('app/css'))
+      .pipe(browserSync.stream())
+}
 
-gulp.task('build', gulp.series('clean', 'export'))
+function build() {
+  return src([
+    'app/css/style.min.css',
+    'app/fonts/**/*',
+    'app/js/main.min.js',
+    'app/*.html'
+  ], {base: 'app'})
+    .pipe(dest('dist'))
+}
 
-gulp.task('default', gulp.parallel('css' ,'scss', 'js', 'browser-sync', 'watch'));
+function watching() {
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/*.html']).on('change', browserSync.reload);
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = cleanDist;
+
+
+exports.build = series(cleanDist, images, build);
+exports.default = parallel(styles ,scripts ,browsersync, watching);
+
+
